17739ad40f2ea5d4dcfef9096268b5d63570a758
Brian Pfeil <brian.pfeil@gmail.com>
2019-11-11 16:04:29 -0500

    remove cast for cxx

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main(int argc, char **argv)
 {
     GSList *list = NULL;
     printf("The list is now %d items long\n", g_slist_length(list));
-    list = g_slist_append(list, (gpointer) "first");
-    list = g_slist_append(list, (gpointer) "second");
+    list = g_slist_append(list, "first");
+    list = g_slist_append(list, "second");
     printf("The list is now %d items long\n", g_slist_length(list));
     g_slist_free(list);
     return 0;
